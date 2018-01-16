a5edc2c0444134263e0044eed848c7d2889731c1
Tom <tom@tomforb.es>
2018-01-16 01:23:38 +0000

    Remove manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,6 @@ module.exports = {
             },
         },
         `gatsby-plugin-feed`,
-        `gatsby-plugin-manifest`,
         `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         {
