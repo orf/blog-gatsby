f6a079b0458298db72490919c3f0a3ba3e6c3b3b
Tom <tom@tomforb.es>
2018-02-24 06:00:57 +0000

    Add gatsby favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,5 +92,23 @@ module.exports = {
         'gatsby-plugin-catch-links',
         `gatsby-plugin-remove-trailing-slashes`,
         `gatsby-plugin-nprogress`,
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        }
     ],
 }
