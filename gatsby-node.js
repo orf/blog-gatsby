1357b0f3d440d942a3fda2025edfe73ab08c5e55
Tom <tom@tomforb.es>
2018-04-15 01:50:24 +0100

    fix conflicting types

--- gatsby-node.js
+++ gatsby-node.js
@@ -81,8 +81,8 @@ exports.createPages = ({graphql, boundActionCreators}) => {
                 createTagPages(createPage, posts, result.data.site.siteMetadata)
 
                 _.each(posts, (post, index) => {
-                    const previous = index === posts.length - 1 ? false : posts[index + 1].node
-                    const next = index === 0 ? false : posts[index - 1].node
+                    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+                    const next = index === 0 ? null : posts[index - 1].node
 
                     createPage({
                         path: post.node.fields.slug,
