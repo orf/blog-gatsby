9cd89a06407c6c60003f9dfb942e982a0a41f4bd
Tom <tom@tomforb.es>
2018-04-15 02:05:38 +0100

    Rework node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,9 +43,9 @@ exports.createPages = ({graphql, boundActionCreators}) => {
 
     return new Promise((resolve, reject) => {
         const blogPost = path.resolve('./src/templates/blog-post.js')
-        resolve(
-            graphql(
-                `
+
+        graphql(
+            `
           {
             site {
               siteMetadata {
@@ -69,33 +69,35 @@ exports.createPages = ({graphql, boundActionCreators}) => {
             }
           }
         `
-            ).then(result => {
-                if (result.errors) {
-                    // eslint-disable-next-line no-console
-                    console.error(result.errors)
-                    reject(result.errors)
-                }
+        ).then(result => {
+            if (result.errors) {
+                // eslint-disable-next-line no-console
+                console.error(result.errors)
+                reject(result.errors)
+            }
 
-                // Create blog posts pages.
-                const posts = result.data.allMarkdownRemark.edges
-                createTagPages(createPage, posts, result.data.site.siteMetadata)
+            // Create blog posts pages.
+            const posts = result.data.allMarkdownRemark.edges
+            createTagPages(createPage, posts, result.data.site.siteMetadata)
 
-                _.each(posts, (post, index) => {
-                    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-                    const next = index === 0 ? null : posts[index - 1].node
+            _.each(posts, (post, index) => {
+                const previous = index === posts.length - 1 ? null : posts[index + 1].node
+                const next = index === 0 ? null : posts[index - 1].node
 
-                    createPage({
-                        path: post.node.fields.slug,
-                        component: blogPost,
-                        context: {
-                            slug: post.node.fields.slug,
-                            previous,
-                            next,
-                        },
-                    })
+                createPage({
+                    path: post.node.fields.slug,
+                    component: blogPost,
+                    context: {
+                        slug: post.node.fields.slug,
+                        previous,
+                        next,
+                    },
                 })
-            })
-        )
+            });
+
+            resolve();
+        })
+
     })
 }
 
